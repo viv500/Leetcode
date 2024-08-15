d201a654fafdb8e14beea6cc05e6b7d903ee80cc
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-08-15 17:51:26 -0400

    Time: 604 ms (65.95%) | Memory: 50.6 MB (41.35%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # better method: single pass using slow and fast pointer (double the speed)
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
         #edge cases
@@ -12,6 +13,25 @@ class Solution:
 
         if not head.next:
             return head.next
+
+        dummyslow = head
+        dummyfast = head
+
+        # won't be null reference cuz we got rid of that. we need this so that slow pointer
+        # is 1 step behind middle element when the fast pointer reaches the end
+        dummyfast = dummyfast.next.next
+
+        # we're doing double skips so we gotta check the next pointer too
+        while dummyfast and dummyfast.next:
+            dummyslow = dummyslow.next
+            dummyfast = dummyfast.next.next
+
+        dummyslow.next = dummyslow.next.next
+
+        return head
+
+
+        ''' issue: 2 separate traversals for length and deletion
 
         length = 0
 
@@ -37,3 +57,4 @@ class Solution:
                 head = head.next
 
         return dummy2
+        '''
