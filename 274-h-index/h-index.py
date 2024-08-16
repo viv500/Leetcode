7c5518166143a9a8f414463ee45ba803d9557c9a
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-08-15 23:22:55 -0400

    Time: 45 ms (32.44%) | Memory: 16.9 MB (55.51%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,6 +1,29 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
 
+        citations.sort(reverse=True)
+        h_index = 0
+
+        # to iterate through indices and list elements together
+        for i, citation in enumerate(citations):
+            # if there are more than i + 1 citations
+            if citation >= i + 1:
+                h_index = i + 1
+
+            # the moment this condiion breaks, you wont see any more cuz of the ordering
+            else:
+                break
+
+        return h_index
+            
+
+        
+        
+        ''' created a hashmap of every value from 0 to max(list)
+        i mapped to how many papers had more than i citations
+        iterated through the hashmap
+        
+        come on bruh... O(n^2) ???
         if not citations:
             return 0
 
@@ -22,5 +45,5 @@ class Solution:
                 h_index = i
 
 
-        return h_index
+        return h_index '''
 
