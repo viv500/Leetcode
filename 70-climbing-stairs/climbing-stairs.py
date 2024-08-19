1f5fa00e8752a908034a3808b5b519e100807e06
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-08-19 17:18:42 -0400

    Time: 30 ms (83.63%) | Memory: 16.5 MB (58.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -9,6 +9,8 @@ class Solution:
         memo[1] = 1 # 1 way to get to 1 step, take 1 step
 
         for i in range(2, n + 1):
+
+            # this makes sense cuz the only ways to get to n are by taking 1 step from n - 1 or 2 steps from n - 2
             memo[i] = memo[i - 1] + memo[i - 2]
         
         return memo[n]
