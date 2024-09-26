33ff0420ef1937f424fce9df35b441767fcbab4e
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-09-26 16:34:29 -0400

    Time: 41 ms (71.87%) | Memory: 12.5 MB (75.54%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
         # 2 pointer would be n log n, not great
-        col = set()
+        col = {}
         for i, j in enumerate(nums):
             if (target - j) in col:
                 return [nums.index(target - j), i]
 
-            col.add(j)
+            col[j] = i
 
