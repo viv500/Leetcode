60173839d1b5b5a946a2b03dc80ef15b0c84ca8b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-03 17:04:00 -0500

    Time: 3 ms (77.43%) | Memory: 12.5 MB (91.48%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,25 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+
+        # stack pushing and popping
 
         stack = []
-        hashmap = {")": "(", "}": "{", "]": "["}
-        
-        for i in s:
-            if i in "{[(":
-                stack.append(i)
-            elif i in hashmap:
-                # Check if the stack is empty or top of stack does not match
-                if not stack or stack[-1] != hashmap[i]:
+        # has to be inverted brackets in dict so we can lookup the opening
+        matching = {"]": "[", "}": "{", ")": "("}
+
+        for char in s:
+            if(char in ["(", "[", "{"]):
+                # no push function
+                stack.append(char)
+            else:
+                # need to check if stack is empty, if so its an immediate fail
+                if (not stack or stack[-1] != matching[char]):
                     return False
                 stack.pop()
-        
-        return len(stack) == 0
+
+        return not len(stack)
