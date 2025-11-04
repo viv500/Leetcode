8a8b43edc29358f7e65afbee454d56a86b665693
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-03 21:56:24 -0500

    Time: 7 ms (19.12%) | Memory: 20.1 MB (20.40%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        
+
+        low = 0
+        high = len(s) - 1
+
+        while low < high:
+            s[low], s[high] = s[high], s[low]
+            low += 1
+            high -= 1
+
+        return s
+
         #damn?
         return s.reverse()
