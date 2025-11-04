2fa20d17e6725b18c6f09d77b8b61c72407a659f
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-04 00:30:48 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (97.62%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        # initialize dummy Node
+        dummy = ListNode()
+        # need to keep the start of this , so store it and use a different node for iterations
+        head = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                head.next = list1
+                list1 = list1.next
+            else:
+                head.next = list2
+                list2 = list2.next
+
+            head = head.next
+
+        if list1:
+            head.next = list1
+        else:
+            head.next = list2
+            
+
+        # we dont want the 0 vertex from constructor
+        return dummy.next
+            
