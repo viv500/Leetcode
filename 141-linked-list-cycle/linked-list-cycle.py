46397e3646fcb189a052dcd14c37acd840c62d04
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-04 00:42:34 -0500

    Time: 49 ms (6.62%) | Memory: 19.5 MB (85.72%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,24 +1,38 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        
+
+        # cycle detection using slow and fast pointer
+        # comparing node1 == node 2 is correct, comparing values is not cuz it can have repeating values
+
         if not head or not head.next:
             return False
 
-        slow = head
-        fast = head
 
-        while fast and fast.next:
+        node1 = head
+        node2 = head.next
 
-            fast = fast.next.next
-            slow = slow.next
 
-            if fast == slow:
+        while node1 and node2:
+            if node1 == node2:
                 return True
 
+            if not node2.next or not node2.next.next:
+                return False # dont want to call next on None, assert this first
+            
+            node1 = node1.next
+            node2 = node2.next.next
+
+
+        # we've reached a terminal node, not a cyclical graph
         return False
-        
