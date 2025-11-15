954698eb2400f3a35ba26c2aee1faf0628f6ab89
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-15 16:19:23 -0500

    Time: 53 ms (5.81%) | Memory: 17.2 MB (8.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,17 +6,57 @@ class Solution(object):
         """
 
         # have pointer for start and end. keep moving end as long as theres a valid overlap. no more overlaps? add [start, end] to array and increment pointers
-        
-        intervals.sort()
+
+
+        def mergesort(array):
+            if len(array) <= 1:
+                return array
+    
+            mid = len(array) // 2
+            left_array = array[:mid]
+            right_array = array[mid:]
+
+            mergesort(left_array)
+            mergesort(right_array)
+
+            i = j = k = 0
+
+            while i < len(left_array) and j < len(right_array):
+                if left_array[i] < right_array[j]:
+                    array[k] = left_array[i]
+                    i += 1
+                else:
+                    array[k] = right_array[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_array):
+                array[k] = left_array[i]
+                i += 1
+                k += 1
+
+            while j < len(right_array):
+                array[k] = right_array[j]
+                j += 1
+                k += 1
+
+
+        mergesort(intervals)
+
         output = []
 
         cur_start = intervals[0][0]
         cur_end = intervals[0][1]
 
+
+        # more consisely, 
+        # if interval[0] <= cur_end::
+        #       cur_end = max(cur_end, interval[1])
+        # no need for elif anymore
         for interval in intervals[1:]: # start with second one
             if interval[1] > cur_end and interval[0] <= cur_end:
                 cur_end = interval[1]
-            elif interval[1] <= cur_end:
+            elif interval[1] <= cur_end: # need this condition incase interval is fully contained or a subset of the cur interval
                 continue
             else:
                 output.append([cur_start, cur_end])
