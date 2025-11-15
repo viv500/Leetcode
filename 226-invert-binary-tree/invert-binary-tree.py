b55dd56dea1c88d4ee6217be833730589350b086
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-15 13:01:00 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (29.55%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -10,25 +10,13 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: Optional[TreeNode]
         """
-        
-        # binary doesnt need to have 2 children
 
         if not root:
-            return None
+            return
 
-
-        #  to avoid wasted space with temp
         root.left, root.right = root.right, root.left
 
-
-        # need self to call any member function
         self.invertTree(root.left)
         self.invertTree(root.right)
 
-
-        # dont need a dummy root cuz we never parse root itself, we just keep sending references of its children back to the recursive call
-
-
         return root
-
-        # O(n)
