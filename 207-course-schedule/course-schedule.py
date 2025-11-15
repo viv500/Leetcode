8bfd62e62562de297c704dce678c22537413f26c
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-15 13:12:31 -0500

    Time: 12 ms (29.92%) | Memory: 16 MB (39.28%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -7,42 +7,44 @@ class Solution(object):
         :rtype: bool
         """
         
-        # dfs approach:
-        # build adjacency matrix with course -> leads to format. call dfs on each course that goes deep into the "leads to" chain
-        # if it finds a vertex its "visiting" -> cycle. if "visited" -> good to go return true. if not visited, mark as visiting.
-        # once we call dfs on all its neighbours (leads to courses) and everything works fine, mark it as visited and return true
+        # [course, prereq]
+        # [1, 0] [0, 1]
 
         UNVISITED = 0
         VISITING = 1
         VISITED = 2
-        states = [UNVISITED] * (numCourses)
 
-        leads = defaultdict(list) # if we try to access an element that doesnt exist, it creates an empty list value for that key
+        states = [UNVISITED] * numCourses
+
+        leads = defaultdict(list)
 
         for leads_to, prereq in prerequisites:
             leads[prereq].append(leads_to)
 
-        def dfs(node):
-            state = states[node]
-            if state == VISITED:
+        def dfs(course):
+            if states[course] == VISITING:
+                return False
+            if states[course] == VISITED:
                 return True
-            if state == VISITING: # cycle found
-                return False        
 
-            # its an unvisited node!
-            states[node] = VISITING
+            # must be unvisited
+            states[course] = VISITING
 
-            for leads_to in leads[node]:
-                if not dfs(leads_to):
+            # visit all the coruses that this course leads to
+            for lead in leads[course]:
+                if not dfs(lead):
                     return False
+
             
-            # no neighbours form a cycle
-            states[node] = VISITED
+            states[course] = VISITED
 
             return True
 
+        
         for course in range(numCourses):
             if not dfs(course):
                 return False
 
         return True
+
+
