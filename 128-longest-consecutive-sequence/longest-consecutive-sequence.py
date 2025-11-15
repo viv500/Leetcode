abdeef95213b4610b7d7290a1c4610e703b8a244
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-15 11:29:56 -0500

    Time: 52 ms (62.46%) | Memory: 27.1 MB (18.90%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -5,6 +5,27 @@ class Solution(object):
         :rtype: int
         """
 
+        # idea: set() uses O(1) loop. confirm an element must be a starting element of a sequence before expanding. only 1 element will expand
+        # may look like O(n^2) but each element is visited exactly once! the inner loop runns for a sequence of element which will never be visited agiab
+        # this is because the inner loop only accesses all consecutive elements within the sequence. if a number isnt a part of this sequence, it will never viist   these again
+
+        num_set = set(nums) # O(n)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set: # can be starting element
+                length = 1
+                cur = num
+                while cur + 1 in num_set:
+                    length += 1
+                    cur += 1
+                
+                longest = max(longest, length)
+
+        return longest
+
+
+
         # brute force: for each item -> search entire array for consecutive elements
         #(O(n^2))
 
@@ -25,25 +46,25 @@ class Solution(object):
 
         # slighty better: sort and then 2 pointer (nlogn)
 
-        nums.sort()
+        # nums.sort()
 
-        if not nums:
-            return 0
+        # if not nums:
+            # return 0
 
-        longest = 0
-        current = 1
+        # longest = 0
+        # current = 1
         
-        for i in range(1, len(nums)):
-            if nums[i - 1] == nums[i]:
-                continue
+        # for i in range(1, len(nums)):
+            # if nums[i - 1] == nums[i]:
+                # continue
 
-            if nums[i - 1] + 1 == nums[i]:
-                current += 1
+            # if nums[i - 1] + 1 == nums[i]:
+                # current += 1
             
-            else:
-                longest = max(longest, current)
-                current = 1 # resent current
+            # else:
+                # longest = max(longest, current)
+                # current = 1 # resent current
 
         
-        return max(longest, current)
+        # return max(longest, current)
 
