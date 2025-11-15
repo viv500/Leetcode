d61b6a650b0669e0effe259c9cb8eb9bc5a76b1b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-15 14:41:20 -0500

    Time: 34 ms (75.02%) | Memory: 19.7 MB (26.99%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -11,30 +11,17 @@ class Solution(object):
         :rtype: bool
         """
 
-        # floyds tortoise and hare method
-        
-
-        # cycle detection using slow and fast pointer
-        # comparing node1 == node 2 is correct, comparing values is not cuz it can have repeating values
-
         if not head or not head.next:
             return False
 
+        slow = head
+        fast = head.next
 
-        node1 = head
-        node2 = head.next
-
-
-        while node1 and node2:
-            if node1 == node2:
+        while(fast.next and fast.next.next):
+            if slow == fast:
                 return True
-
-            if not node2.next or not node2.next.next:
-                return False # dont want to call next on None, assert this first
-            
-            node1 = node1.next
-            node2 = node2.next.next
+            slow = slow.next
+            fast = fast.next.next
 
 
-        # we've reached a terminal node, not a cyclical graph
         return False
