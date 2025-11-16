3e139139b0c297b5b214142f442b51b042b06f35
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-16 14:55:55 -0500

    Time: 42 ms (83.37%) | Memory: 13.2 MB (82.61%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        
+        # edge case
+        if len(s) < len(p):
+            return []
+        
+        # hashmap for anagrams and sliding window to check intervals
+        freq = [0] * 26
+        for char in p:
+            index = ord(char) - ord('a')
+            freq[index] += 1
+        
+        output = []
+        window_size = len(p)
+
+
+        # initializing hashmap for each window
+        freq_inner = [0] * 26
+        for char in s[:window_size]: # upper bound exclusive even for slicing
+            index = ord(char) - ord('a')
+            freq_inner[index] += 1
+        
+        p1 = 0
+        p2 = window_size # p2 is ahead but its what we wanna "add" for the next round
+        
+        while p2 < len(s):
+            if freq_inner == freq:
+                output.append(p1)
+            
+            freq_inner[ord(s[p1]) - ord('a')] -= 1
+            freq_inner[ord(s[p2]) - ord('a')] += 1
+            
+            p1 += 1
+            p2 += 1
+        
+        # Check the last window
+        if freq_inner == freq:
+            output.append(p1)
+        
+        return output
