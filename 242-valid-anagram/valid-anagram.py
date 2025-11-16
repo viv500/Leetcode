f5b38d12b73889623292ffd0a2747ba75cf46254
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-16 14:02:37 -0500

    Time: 14 ms (87.47%) | Memory: 12.5 MB (82.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,33 @@
-from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        # optimal space: use 1 dictionary and decrement in second round
+        # space is O(1) cuz the number of alphabets is fixed. there can only be upto 26 hashmap entries
+        # hashmap
+
+        # checks avoids extra space
+        if len(s) != len(t):
+            return False
+            
+        chars = {}
+
+        for character in s:
+            if character in chars:
+                chars[character] += 1
+            else:
+                chars[character] = 1
+
+        for character in t:
+            if character not in chars or chars[character] < 1:
+                return False
+            else:
+                chars[character] -= 1
+
+        return True
+
+        
