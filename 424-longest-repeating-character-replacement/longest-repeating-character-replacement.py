c9b943702bd615768a324c76018f3dffa387d398
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-18 12:55:39 -0500

    Time: 333 ms (8.25%) | Memory: 12.7 MB (95.74%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,26 +6,25 @@ class Solution(object):
         :rtype: int
         """
         
-        # Q) longest "same character" substring where you can change any k characters
-        # sliding window with frequency array. max(frequency array) = the count of the "same character" we want
-        # we dont care what the character is, just that it is what we want to flip other characters into
-        # logic to find invalid window: if size of current window - max(freq array) > k 
-        # i.e. we need to flip more character than we have flips available (k)
+        # idea -> pick the character that has most occurances and try flipping others
+        # condition for success -> if number of character that are not our majoirt character is <= k, this is possible!
 
+        # initialize frequency table
         freq = [0] * 26
-        L = 0
-        longest = 0
 
-        for R in range(len(s)):
-            freq[ord(s[R]) - ord('A')] += 1 # check if window is valid after expanding
-    
+        L = R = 0
+        longest = -1
+
+        while R < len(s):
+
+            freq[ord(s[R]) - ord('A')] += 1
+            
+            # condition for moving left window
             while((R - L + 1) - max(freq) > k):
-                freq[ord(s[L]) - ord('A')] -= 1 # its A here not a
+                freq[ord(s[L]) - ord('A')] -= 1
                 L += 1
 
             longest = max(longest, R - L + 1)
+            R += 1
 
         return longest
-            
-        # Time: O(n)
-        # Space: O(1)
