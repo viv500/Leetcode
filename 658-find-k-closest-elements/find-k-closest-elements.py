647ec2aa1425cbf6c555b96840533aeed65e2097
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-17 22:02:18 -0500

    Time: 0 ms (100.00%) | Memory: 13.6 MB (74.27%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -7,7 +7,24 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # 2 pointer approach -> shrink based on side that has closer element to k O(n - k)
+        # binary search approach: O(log(n - k) + k)
+        # binary searching on starting positions for the range
+        # We don't need abs because we're not measuring distances, we're checking which side of the window x is leaning towards:
+
+        L = 0
+        R = len(arr) - k # left most staring position for window
+
+        while L < R:
+            mid = (L + R )// 2
+            if x - arr[mid] > arr[mid + k] - x:
+                L = mid + 1
+            else:
+                R = mid
+
+        return arr[L:L+k]
+
+
+        # 2 pointer sliding window approach -> shrink based on side that has closer element to k O(n - k)
 
         L = 0
         R = len(arr) - 1
