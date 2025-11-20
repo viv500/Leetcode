71be862137c7f4ce2ea8a7e38a99b9e65a673e54
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-19 23:51:24 -0500

    Time: 391 ms (5.97%) | Memory: 17.3 MB (8.64%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -9,28 +9,25 @@ class Solution(object):
         # if you remove element from the set when shrinking window, you don't really know if theres more occurances in current window
         # ex. [3,3,3,1,2,1,1,2,3,3,4]
         # condition is met when you set.remove(3) so shrinking stops. but 3 is still in thee window!!
+        # "1 fruit from each tree" -> contiguous block
 
-        if len(set(fruits)) == 1 or len(set(fruits)) == 2: # only 1 or 2 fruit types
-            return len(fruits)
-    
-        
-        L = R = 0
-        longest = 0
-        baskets = defaultdict(int) # cleaner code, initializes all values as 0
+        if len(set(fruits)) == 1: return len(fruits) # can pick all
 
-        while(R < len(fruits)):
+        baskets = defaultdict(int)
+
+        R = L = maximum = 0
+
+        while R < len(fruits):
             baskets[fruits[R]] += 1
-
             while(len(baskets) > 2):
                 baskets[fruits[L]] -= 1
-
                 if baskets[fruits[L]] == 0:
-                    baskets.pop(fruits[L]) # IMPORTANT: deleting a key from dictionary so length stays the same
-                    
+                    baskets.pop(fruits[L])
                 L += 1
 
-            longest = max(longest, R - L + 1)
-
+            maximum = max(maximum, R - L + 1)
             R += 1
+        return maximum
+            
 
-        return longest
+       
