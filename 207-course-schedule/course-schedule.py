a8eee3fc7d14e0a263da3768fd9f32d76e519281
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-20 11:34:47 -0500

    Time: 7 ms (46.64%) | Memory: 19.2 MB (61.52%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,42 +1,46 @@
 from collections import defaultdict
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
-        graph = defaultdict(list)
+        leads_to = defaultdict(list)
 
-        for prereq, leads_to in prerequisites:
-            graph[prereq].append(leads_to)
+        # adjacency hashmap mapping course -> course it leads to
+        for course, prereq in prerequisites:
+            leads_to[prereq].append(course)
 
-        UNVISITED, VISITING, VISITED = 0, 1, 2
-        states = [UNVISITED] * numCourses
+        unvisited, visiting, visited = 0, 1, 2
+        # if a node is "visited", its prereqs have been resolved 
+        # if its in "visiting" we are currently resolving its prereq and leads to chain, if we rediscover a "visiting" node, there
+        # is a cycle in its dependancies
 
-        def dfs(node):
-            if states[node] == VISITED:
+        # the state of courses
+        states = [unvisited] * numCourses
+
+
+        def dfs(course):
+            if states[course] == 2:
                 return True
-            elif states[node] == VISITING:
+            if states[course] == 1:
                 return False
 
-            
-            # we are now visiting
-            states[node] = VISITING
+            # add to visiting
+            states[course] = 1
 
-            for leads_to in graph[node]:
-                if not dfs(leads_to):
+            for lead_to in leads_to[course]:
+                # if any returns false, cycle in depednancies. if true, we can continue checking
+                if not dfs(lead_to):
                     return False
 
-            # everything went okay and we're back to the original course
-            states[node] = VISITED
-            return True
+            # all dependancies resolved
+            states[course] = 2
 
-        for course in range(numCourses):
-            if not dfs(course):
+            return True # need this or python my implicitly return 0 (False)
+
+
+        for c in range(numCourses):
+            if not dfs(c):
                 return False
 
         return True
-    
-
+                
+            
