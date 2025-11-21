d51eaf58ff2451112c121855512ab061cb062801
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-21 18:30:42 -0500

    Time: 16 ms (73.12%) | Memory: 18.5 MB (90.40%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,16 +1,58 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
-        # DP solution (O(n^2))
+        # approach: greedy
+        # time: O(n)
+        # space: O(1)
+        # intuition: dynamically change our target if we know that a target can get to n - 1
+        # can reach if our target eventually reaches 0
 
-        # greedy: keep shifting the goal backwards to make the final target simple
+        n = len(nums)
+        target = n - 1
 
-        goal = len(nums) - 1 # goal is intially at the end
+        for i in range(n - 1, -1, -1):
+            max_jump = nums[i]
+            # >= target implies we can reach our target
+            if i + nums[i] >= target:
+                target = i
 
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] + i >= goal: #nums[i] is the jump length, i is the current position
-                goal = i # move the goal back to a closer possible goal
+        return target == 0
 
-        return goal == 0 # only if we can reach the 0th index, the path is possible
+        # approach: top down DP (memoization)
+        # time: (O(n)^2)
+        # space: O(n) for recursive call stack
 
-        
+        n = len(nums)
+        memo = {n - 1: True}
+        # starting from index i, can i reach the last index?
+        def canReach(i):
+            if i in memo:
+                return memo[i]
+
+            for jump in range(1, nums[i] + 1):
+                if canReach(i + jump):
+                    memo[i + jump] = True
+                    return True
+
+            memo[i] = False
+            return False
+
+        return canReach(0)
+
+        # approach: recursive
+        # time: (O(max nums)^n)
+        # space: O(n) for recursive call stack
+
+        n = len(nums)
+        # starting from index i, can i reach the last index?
+        def canReach(i):
+            if i == n - 1:
+                return True
+
+            for jump in range(1, nums[i] + 1):
+                if canReach(i + jump):
+                    return True
+
+            return False
+
+        return canReach(0)
