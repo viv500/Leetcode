9206bef81358af6c431deba17d9cdefb635382cc
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-21 11:52:50 -0500

    Time: 3 ms (97.32%) | Memory: 20.7 MB (56.22%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -2,6 +2,7 @@ class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         # 2 pointer approach
         # sorting helps form contiguous blocks of common prefix words O(n log n)
+        # Time = O(nlogn + m + n) where n = size of products, m = size of searchword
         products.sort()
 
         L, R = 0, len(products) - 1
@@ -9,6 +10,8 @@ class Solution:
 
         for i in range(len(searchWord)):
             word = searchWord[i]
+             # need to check L doesnt cross R and
+             # if either theres a character mismatch or the word is too short, skip
             while L <= R and (len(products[L]) <= i or products[L][i] != word):
                 L += 1
 
