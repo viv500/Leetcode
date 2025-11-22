c88960078232c74908bc5f1c023af0c355d9219d
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 16:20:10 -0500

    Time: 3 ms (77.08%) | Memory: 19.4 MB (42.02%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,52 +1,39 @@
 from collections import defaultdict
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        
-        # topological sort !!
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        leads_to = defaultdict(list)
+        output = []
 
-        UNVISITED, VISITING, VISITED = 0, 1, 2
-        states = [UNVISITED] * numCourses
+        for course, lead_to in prerequisites:
+            leads_to[course].append(lead_to)
 
-        graph = defaultdict(list)
-
-        for prereq, leads_to in prerequisites:
-            graph[prereq].append(leads_to)
-
-        topo_order = []
+        states = [0] * numCourses
+        visited = 2
+        visiting = 1
+        unvisited = 0
 
         def dfs(course):
-            state = states[course]
-
-            if state == VISITING:
-                return False
-            elif state == VISITED:
+            if states[course] == visited:
                 return True
+            if states[course] == visiting:
+                return False
 
-            states[course] = VISITING
+            states[course] = visiting
 
-            for leads_to in graph[course]:
-                if not dfs(leads_to):
+            for courses in leads_to[course]:
+                if not dfs(courses):
                     return False
 
-            # the first course to reach this is the one with the least dependancies
-            # others are sitll in nested recursive calls
-            # adding courses here will preserve topoligcal order
-
-            states[course] = VISITED
-            topo_order.append(course)
-
-            # to tell dfs that dependancies are resolved
+            states[course] = visited
+            output.append(course)
             return True
 
-        
         for course in range(numCourses):
             if not dfs(course):
                 return []
 
-        # by this point, all courses can be taken
-        return topo_order
+
+        return output
+
+
+        
