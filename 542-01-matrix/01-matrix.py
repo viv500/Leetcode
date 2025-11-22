f786a2887aed90c16db48944c1208bc023b7ac4e
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 15:58:49 -0500

    Time: 147 ms (49.53%) | Memory: 20.5 MB (63.77%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,45 +1,27 @@
 from collections import deque
-class Solution(object):
-    def updateMatrix(self, mat):
-        """
-        :type mat: List[List[int]]
-        :rtype: List[List[int]]
-        """
-
-        # find all 0s
-        # need a visited set cuz non 0 cells could be visited, no other way to check
-        # NOTE: cant create set of lists (sets are immutable) add tuples instead
-        # time: O(n^2)
-        # space O(n^2)
-
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         visited = set()
-
         rows, cols = len(mat), len(mat[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
         q = deque()
 
         for row in range(rows):
             for col in range(cols):
                 if mat[row][col] == 0:
-                    q.append([row, col])
-                    visited.add((row, col))
+                    q.append((row, col))
+                    
 
-
-        # multi source bfs
         while q:
-            level_size = len(q)
+            r, c = q.popleft()
 
-            for _ in range(level_size):
-                r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
 
-                directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+                if 0 <= nr < rows and 0 <= nc < cols and mat[nr][nc] != 0 and (nr, nc) not in visited:
+                    q.append((nr, nc))
+                    visited.add((nr, nc))
+                    mat[nr][nc] = mat[r][c] + 1
 
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-
-                    if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
-                        mat[nr][nc] = mat[r][c] + 1 # shortest path
-                        q.append([nr, nc])
-                        visited.add((nr, nc))
         return mat
-
