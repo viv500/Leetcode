81613792b68493e194c8c02f9bb1525c30915af1
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 15:41:28 -0500

    Time: 233 ms (84.41%) | Memory: 20.1 MB (82.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,5 +1,7 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
         def dfs(r, c):
             if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
@@ -14,7 +16,17 @@ class Solution:
 
             
         def bfs(r, c):
-            pass
+            q = deque([(r, c)])   
+
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + r, dc + c
+
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
+                        grid[nr][nc] = "0"
+                        q.append((nr, nc))
+
 
 
         rows, cols = len(grid), len(grid[0])
@@ -24,7 +36,7 @@ class Solution:
             for col in range(cols):
                 if grid[row][col] == "1":
                     islands += 1
-                    dfs(row, col)
+                    bfs(row, col)
 
         return islands
 
