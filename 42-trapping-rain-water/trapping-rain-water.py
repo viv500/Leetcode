d6cccd0ecb7af6f22d47dc1c574cb28b15b771cb
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 12:46:16 -0500

    Time: 31 ms (19.80%) | Memory: 19.3 MB (69.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,37 +1,20 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        max_left = max_right = 0
+        left = [0] * len(height)
+        right = [0] * len(height)
+        summ = 0
+
+        for i in range(len(height)):
+            left[i] = max_left
+            max_left = max(max_left, height[i])
+
+        for i in range(len(height) - 1, -1, -1):
+            right[i] = max_right
+            max_right = max(max_right, height[i])
+
+        for i in range(len(height)):
+            summ += max(0, min(left[i], right[i]) - height[i])
+
+        return summ
         
-        # idea : array of max value seen the the left for each value (excluding self)
-        # array of max value seen the the right for each value (excluding self)
-        # potential water for each spot = min(max l value, max r value)
-        # actual water = potential water - height
-        # if actual < 0: actual = 0
-
-        left_array = []
-        right_array = []
-        
-        max_left = 0
-        max_right = 0
-
-        for h in range(len(height)):
-            left_array.append(max_left)
-            max_left = max(max_left, height[h])
-
-        for h in range(len(height) - 1, -1, -1):
-            right_array.append(max_right)
-            max_right = max(max_right, height[h])
-
-        right_array.reverse()
-
-        sum = 0
-        for i in range(len(left_array)):
-            sum += max(0, min(left_array[i], right_array[i]) - height[i]) # avoids negatives
-
-        return sum
-        
-        print(right_array)
-
