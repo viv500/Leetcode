d81822f15631328e0f5b2f242ca215bd77a8cab7
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 01:49:07 -0500

    Time: 7 ms (50.39%) | Memory: 24.3 MB (7.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,8 +2,29 @@ import heapq
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # O(n log n)
         count = Counter(nums)
+
+        # O(n) bucket sort with O(n) space
+        buckets = [[] for _ in range(len(nums) + 1)] # extra for 0-bucket and n-bucket
+
+        for value, amount in count.items():
+            buckets[amount].append(value) # add the value to the bucket for its specific amount
+
+        output = []
+        for i in range(len(nums), -1, -1):
+            output.extend(buckets[i])
+
+            if len(output) > k:
+                return output[:k]
+
+        
+
+
+
+
+        # O(n log k) good when is k << n (heap size never bigger than k)
+        # space: O(n)
+        # min heap approahc -> stack size exceeds k? remove least occuring (min)
 
         heap = []
         for num, amount in count.items():
@@ -14,14 +35,12 @@ class Solution:
 
         return [num for value, num in heap]
 
+    
+        # O(n log n)
+        # space: O(n)
         elements = list(count.items())
         elements.sort(key=lambda t: t[1])
         return [e[0] for e in elements[-1:-k-1:-1]]
-
-        # O(n log k) great is k << n (heap size never bigger than k)
-        # min heap approahc -> stack size exceeds k? remove least occuring (min)
-        
-
 
 
 
