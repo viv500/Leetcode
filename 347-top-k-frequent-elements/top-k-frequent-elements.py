098d16f57e46f3d35f390c6e6041e2fff7b1dd42
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 01:39:12 -0500

    Time: 6 ms (54.65%) | Memory: 21.3 MB (39.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,8 +4,24 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # O(n log n)
         count = Counter(nums)
+
+        heap = []
+        for num, amount in count.items():
+            heapq.heappush(heap, (amount, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for value, num in heap]
+
         elements = list(count.items())
         elements.sort(key=lambda t: t[1])
         return [e[0] for e in elements[-1:-k-1:-1]]
 
+        # O(n log k) great is k << n (heap size never bigger than k)
+        # min heap approahc -> stack size exceeds k? remove least occuring (min)
         
+
+
+
+
