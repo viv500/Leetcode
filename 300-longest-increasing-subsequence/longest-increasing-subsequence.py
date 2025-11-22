d20ba99e269e39e2cd70319f1ad26c569303e3a2
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 16:49:37 -0500

    Time: 1814 ms (22.59%) | Memory: 17.9 MB (79.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,16 +1,9 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # intuition: dp[i] stores longest subsequence ending at ith index
-        # Time: O(n^2) and O(n) space complexity
-
-        n = len(nums)
-        dp = [1] * n # initialise all indices having longest subsequence 1 (itself)
-
-        for i in range(n):
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
             for j in range(i):
-                if nums[i] > nums[j]: # if longer subsequence can be formed
-                # longest subsequence is either current of the one formed at j, + 1 step to i
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        # the longest one doesnt end at dp[n - 1]
+
         return max(dp)
-        
