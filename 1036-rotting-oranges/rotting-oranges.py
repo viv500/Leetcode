2efb7149766482bd5fddea6b51aa78fa3c41d020
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-23 15:28:26 -0500

    Time: 3 ms (81.41%) | Memory: 17.8 MB (43.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,43 +1,32 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # multi source bfs
-        # need a for loop so we can increment counter level by level
-
         rows, cols = len(grid), len(grid[0])
-        time = 0
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
         rotten = deque()
+        time = 0
         fresh_orange_count = 0
 
-        # collect all the rotten oranges so we can call bfs on them
-        # O(n^2)
         for row in range(rows):
             for col in range(cols):
-                # rotten orange!!
-                if grid[row][col] == 2:
-                    rotten.append((row, col))
                 if grid[row][col] == 1:
                     fresh_orange_count += 1
+                if grid[row][col] == 2:
+                    rotten.append((row, col))
 
-        
-        while(rotten and fresh_orange_count > 0):
+        while rotten and fresh_orange_count > 0:
             level_size = len(rotten)
 
             for _ in range(level_size):
                 r, c = rotten.popleft()
 
-                directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
-
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2 # mark rotten
-                        rotten.append((nr, nc))
-                        # 1 less fresh orange
                         fresh_orange_count -= 1
+                        rotten.append((nr, nc))
+                        grid[nr][nc] = 0 # mark visited
             time += 1
 
-        return time if fresh_orange_count == 0 else -1
-
-        return 
+        return -1 if fresh_orange_count != 0 else time
 
