2f068333e8bc304d2356a523af2a01f525177631
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-22 19:05:49 -0500

    Time: 3 ms (36.48%) | Memory: 21.1 MB (72.25%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,20 +1,13 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: Optional[TreeNode]
-        :type k: int
-        :rtype: int
-        """
-
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         stack = []
         current = root
-
         while stack or current:
             while current:
                 stack.append(current)
@@ -27,22 +20,6 @@ class Solution(object):
 
             current = current.right
 
-        # while stack or current ensures that we continue traversing as long as:
-        # There are nodes in the stack waiting to be processed OR
-        # We have a current node to dive down from
-        # need to do the deep left exploration even for left nodes
-
-
-
-
-        # O(n) time and space => space optimal but not time optimal
-
-        # def inorder_traversal(node):
-            # if not node:
-                # return []
-
-            # return inorder_traversal(node.left) + [node.val] + inorder_traversal(node.right)
 
         
-        # sorted_elements = inorder_traversal(root)
-        # return sorted_elements[k - 1]
+    
