2fa28e435b89a71fbef7214c0a4c4c657e4ad6e5
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-24 21:19:46 -0500

    Time: 230 ms (13.78%) | Memory: 18 MB (48.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,30 +1,17 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        
-        # idea -> pick the character that has most occurances and try flipping others
-        # condition for success -> if number of character that are not our majoirt character is <= k, this is possible!
-
-        # initialize frequency table
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        L = R = 0
+        longest = 0
         freq = [0] * 26
 
-        L = R = 0
-        longest = -1
-
         while R < len(s):
-
             freq[ord(s[R]) - ord('A')] += 1
-            
-            # condition for moving left window
+
             while((R - L + 1) - max(freq) > k):
                 freq[ord(s[L]) - ord('A')] -= 1
                 L += 1
 
             longest = max(longest, R - L + 1)
-            R += 1
 
+            R += 1
         return longest
