472db0478d6d6d4a16a630bd9b1135d96fe15a3b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-11-24 21:29:47 -0500

    Time: 4 ms (46.65%) | Memory: 17.7 MB (70.87%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,19 +2,18 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        rotten = deque()
-        time = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         fresh_orange_count = 0
-
+        minutes = 0
+        rotten = deque()
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1:
-                    fresh_orange_count += 1
                 if grid[row][col] == 2:
                     rotten.append((row, col))
+                if grid[row][col] == 1:
+                    fresh_orange_count += 1
 
-        while rotten and fresh_orange_count > 0:
+        while(rotten and fresh_orange_count > 0):
             level_size = len(rotten)
 
             for _ in range(level_size):
@@ -22,11 +21,23 @@ class Solution:
 
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
+
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                        fresh_orange_count -= 1
+                        grid[nr][nc] = 2
+                        fresh_orange_count -=1
                         rotten.append((nr, nc))
-                        grid[nr][nc] = 0 # mark visited
-            time += 1
+            minutes += 1
 
-        return -1 if fresh_orange_count != 0 else time
 
+        return minutes if fresh_orange_count == 0 else -1
+
+
+
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    max_area = max(max_area, bfs(row, col))
+
+        return max_area
