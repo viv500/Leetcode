ba9c333cee033089c9a9c6fa7ec3cc1366ec9812
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2025-12-12 01:20:58 -0500

    Time: 7 ms (46.36%) | Memory: 19.4 MB (40.16%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,46 +1,29 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
         leads_to = defaultdict(list)
+        for course, lead_to in prerequisites:
+            leads_to[course].append(lead_to)
 
-        # adjacency hashmap mapping course -> course it leads to
-        for course, prereq in prerequisites:
-            leads_to[prereq].append(course)
-
-        unvisited, visiting, visited = 0, 1, 2
-        # if a node is "visited", its prereqs have been resolved 
-        # if its in "visiting" we are currently resolving its prereq and leads to chain, if we rediscover a "visiting" node, there
-        # is a cycle in its dependancies
-
-        # the state of courses
-        states = [unvisited] * numCourses
-
+        states = [0] * numCourses
 
         def dfs(course):
-            if states[course] == 2:
-                return True
             if states[course] == 1:
                 return False
+            if states[course] == 2:
+                return True
 
-            # add to visiting
             states[course] = 1
 
             for lead_to in leads_to[course]:
-                # if any returns false, cycle in depednancies. if true, we can continue checking
                 if not dfs(lead_to):
                     return False
 
-            # all dependancies resolved
             states[course] = 2
+            return True
 
-            return True # need this or python my implicitly return 0 (False)
-
-
-        for c in range(numCourses):
-            if not dfs(c):
+        for course in range(numCourses):
+            if not dfs(course):
                 return False
 
         return True
-                
-            
