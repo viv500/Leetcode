d8698f1d6efc6178fb07c79bb9fbe97fda928cde
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-23 16:03:16 -0500

    Time: 20 ms (58.02%) | Memory: 19.2 MB (42.63%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,37 +2,34 @@ from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        max_area = 0
+        directions = ((-1, 0), (1, 0), (0, 1), (0, -1))
+        max_size = 0
 
         def bfs(r, c):
-            area = 1
-
+            size = 1
             q = deque([(r, c)])
             grid[r][c] = 0
 
             while q:
                 r, c = q.popleft()
-
+    
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
-
-                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                        area += 1
-                        grid[nr][nc] = 0
+                    
+                    if(0 <= nc < cols and 0 <= nr < rows and grid[nr][nc] == 1):
+                        size += 1
                         q.append((nr, nc))
-            return area
+                        grid[nr][nc] = 0
 
+            return size
 
-
-
-
+        
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
-                    max_area = max(max_area, bfs(row, col))
-
-        return max_area
+                    max_size = max(max_size, bfs(row, col))
 
 
+        return max_size
 
+            
