dca9f204550cc1c43c7c0d96e6ddf0d187881cea
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-25 12:34:15 -0500

    Time: 7 ms (45.16%) | Memory: 21.5 MB (7.78%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,10 +2,10 @@ from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         leads_to = defaultdict(list)
-        for course, lead_to in prerequisites:
-            leads_to[course].append(lead_to)
+        states = numCourses * [0]
 
-        states = [0] * numCourses
+        for second, first in prerequisites:
+            leads_to[first].append(second)
 
         def dfs(course):
             if states[course] == 1:
@@ -15,15 +15,18 @@ class Solution:
 
             states[course] = 1
 
-            for lead_to in leads_to[course]:
-                if not dfs(lead_to):
+            for lead in leads_to[course]:
+                if not dfs(lead):
                     return False
 
             states[course] = 2
+
             return True
 
+        
         for course in range(numCourses):
             if not dfs(course):
                 return False
+
 
         return True
