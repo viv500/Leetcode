e7d35d73a9df5141cd85559bf667f33084fe2ced
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-26 16:18:26 -0500

    Time: 131 ms (79.64%) | Memory: 19.8 MB (34.00%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,36 +1,32 @@
 from collections import deque
-class Solution(object):
-    def shortestPathBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        
-        if not grid or not grid[0]: return -1
-        if grid == [[0]]: return 1
-
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-
-        # edge cases! top left and bottom right are not 0s
-        if grid[0][0] != 0 or grid[rows - 1][cols - 1] != 0: return -1
-
+        if grid[0][0] == 1 or grid[rows - 1][cols - 1] == 1: return -1
+        if grid == [[0]]: return 1
+       
         q = deque([(0, 0)])
-        grid[0][0] = 1
+        directions = [[0, 1], [0, -1], [-1, 0], [1, 0], [-1, -1], [1, 1], [1, -1], [-1, 1]]
 
-        while(q):
+        while q:
             r, c = q.popleft()
 
-            directions = [[-1, 0], [-1, -1], [-1, 1], [1, 0], [1, -1], [1, 1], [0, -1], [0, 1]]
-
             for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-
+                nr, nc = dr + r, dc + c
                 if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
-                    # did we reach bottom right?
-                    if (nr == rows - 1 and nc == cols - 1):
-                        return grid[r][c] + 1
-                    else:
-                        grid[nr][nc] = grid[r][c] + 1
-                        q.append((nr, nc))
+                    if (nr, nc) == (rows - 1, cols - 1): 
+                        return grid[r][c] + 2
+
+                    # no need to commpare shortest distance since if grid[nr][nc] = 0, then this IS the
+                    # shortest path to this cell (bfs)
+                    
+                    grid[nr][nc] = grid[r][c] + 1
+                    q.append((nr, nc))
 
         return -1
+
+
+
+
+
+    
