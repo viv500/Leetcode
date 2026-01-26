e60118332b89d14c243f5f8752d5414ae0f7b0f4
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-25 21:06:59 -0500

    Time: 123 ms (64.61%) | Memory: 21.4 MB (40.38%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,27 +1,27 @@
 from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        visited = set()
         rows, cols = len(mat), len(mat[0])
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
         q = deque()
 
         for row in range(rows):
             for col in range(cols):
                 if mat[row][col] == 0:
                     q.append((row, col))
-                    
+                else:
+                    mat[row][col] = float('inf')
 
         while q:
             r, c = q.popleft()
 
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
-
-                if 0 <= nr < rows and 0 <= nc < cols and mat[nr][nc] != 0 and (nr, nc) not in visited:
-                    q.append((nr, nc))
-                    visited.add((nr, nc))
+                if 0 <= nr < rows and 0 <= nc < cols and mat[r][c] + 1 < mat[nr][nc]:
                     mat[nr][nc] = mat[r][c] + 1
+                    q.append((nr, nc))
 
         return mat
+
+        
+
