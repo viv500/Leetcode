89055a39269e68e402aa0f330f96455967bcde6f
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-27 23:45:19 -0500

    Time: 8 ms (45.95%) | Memory: 23.2 MB (11.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,68 +1,22 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-
-        # have pointer for start and end. keep moving end as long as theres a valid overlap. no more overlaps? add [start, end] to array and increment pointers
-
-
-        def mergesort(array):
-            if len(array) <= 1:
-                return array
-    
-            mid = len(array) // 2
-            left_array = array[:mid]
-            right_array = array[mid:]
-
-            mergesort(left_array)
-            mergesort(right_array)
-
-            i = j = k = 0
-
-            while i < len(left_array) and j < len(right_array):
-                if left_array[i] < right_array[j]:
-                    array[k] = left_array[i]
-                    i += 1
-                else:
-                    array[k] = right_array[j]
-                    j += 1
-                k += 1
-
-            while i < len(left_array):
-                array[k] = left_array[i]
-                i += 1
-                k += 1
-
-            while j < len(right_array):
-                array[k] = right_array[j]
-                j += 1
-                k += 1
-
-
-        mergesort(intervals)
-
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sorting allows for 1 pass
+        intervals.sort()
         output = []
 
         cur_start = intervals[0][0]
         cur_end = intervals[0][1]
 
+        for interval in intervals[1:]:
+            next_start = interval[0]
+            next_end = interval[1]
 
-        # more consisely, 
-        # if interval[0] <= cur_end::
-        #       cur_end = max(cur_end, interval[1])
-        # no need for elif anymore
-        for interval in intervals[1:]: # start with second one
-            if interval[1] > cur_end and interval[0] <= cur_end:
-                cur_end = interval[1]
-            elif interval[1] <= cur_end: # need this condition incase interval is fully contained or a subset of the cur interval
-                continue
+            if next_start <= cur_end:
+                cur_end = max(next_end, cur_end)
             else:
                 output.append([cur_start, cur_end])
-                cur_start = interval[0] #modify pointers, we're done with the previous interval
-                cur_end = interval[1]
-
-        output.append([cur_start, cur_end]) # last one gets skipped
+                cur_start = next_start
+                cur_end = next_end
+        output.append([cur_start, cur_end])
 
         return output
