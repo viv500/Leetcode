68a4d069768350e4c5e2fbda32668b4ba40f5b75
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-28 17:15:03 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (15.67%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,38 +1,36 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
+        output = []
+        rows, cols = len(matrix), len(matrix[0])
 
-        result = []
+        top, bottom, left, right = 0, rows, 0, cols
 
-        while left <= right and top <= bottom:
-            # left -> right
-            for c in range(left, right + 1):
-                result.append(matrix[top][c])
+        while left < right and top < bottom:
+
+            # left to right
+            for i in range(left, right):
+                output.append(matrix[top][i])
 
             top += 1
+            # top to bottom
+            for i in range(top, bottom):
+                output.append(matrix[i][right - 1])
 
-            # top -> bottom
-            for r in range(top, bottom + 1):
-                result.append(matrix[r][right])
+            right -=1
 
-            right -= 1
+            # right to left
+            if top < bottom:
+                for i in range(right - 1, left - 1, -1):
+                    output.append(matrix[bottom - 1][i])
 
-            if top <= bottom:
-                # right -> left
-                for c in range(right, left - 1, -1):
-                    result.append(matrix[bottom][c])
+                bottom -= 1
 
-                bottom -= 1 
+            # bottom to top
+            if left < right:
+                for i in range(bottom - 1, top - 1, -1):
+                    output.append(matrix[i][left])
 
-            if left <= right:
-                # bottom -> top
-                for r in range(bottom, top - 1, -1):
-                    result.append(matrix[r][left])
+                left += 1
 
-                left += 1 
 
-        return result
-
-            
-
+        return output
