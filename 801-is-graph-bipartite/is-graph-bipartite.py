4214cb5d0f41055615a7d540ba808b5860e6b08b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-01-28 22:03:30 -0500

    Time: 0 ms (100.00%) | Memory: 20.8 MB (7.85%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,37 +1,28 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        # 2 colouring algoirthm
-        # if we encouter an already coloured node and its not the coloour we need
-        # odd cycle ! since we started from a source node and ended on the same node too which
-        # forms an odd length cycle <=> not bipartite
 
-        # call dfs/bfs on every node if uncoloured. uncoloured means its on a separate component
-        colour = ['white'] * len(graph)
+        colours = ['white'] * len(graph)
 
-        def dfs(node):
-            if colour[node] == 'white':
-                colour[node] = 'red'
-            nei_colour = 'red' if colour[node] == 'blue' else 'blue'
+        def dfs(vertex):
+            if colours[vertex] == 'white':
+                colours[vertex] = 'red'
 
-            for nei in graph[node]:
-                if colour[nei] == 'white':
-                    colour[nei] = nei_colour
-                    if not dfs(nei):
+            complement = 'red' if colours[vertex] == 'blue' else 'blue'
+
+            for nei in graph[vertex]:
+                if colours[nei] == 'white':
+                    colours[nei] = complement
+                    if not dfs(nei): 
                         return False
-
-                elif colour[nei] == nei_colour:
-                    continue
-                else:
-                    print(node)
-                    print(nei)
+                elif colours[nei] != complement:
                     return False
+                else:
+                    continue
 
             return True
 
-
-        for node in range(len(graph)):
-            if colour[node] == 'white':
-                if not dfs(node): return False
+        for vertex in range(len(graph)):
+            if colours[vertex] == 'white':
+                if not dfs(vertex): return False
 
         return True
-
