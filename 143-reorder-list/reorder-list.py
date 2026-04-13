a1bdf7e58ac15f5f5587c491504bd54222817944
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-13 19:17:17 -0400

    Time: 7 ms (16.23%) | Memory: 27.7 MB (84.50%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -7,50 +7,44 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
+
+        Approach:
+          1. Find the middle of the list using fast/slow pointers, and split
+             the list into two halves. For odd-length lists, the middle node
+             stays with the first half.
+          2. Reverse the second half.
+          3. Interleave nodes from the first half with the reversed second half.
         """
-        # note: midpoint for odd length -> middle is included in left
-        
-        # 1. find middle of list using fast-slow pointers (need to cut in half)
-        # 2. reverse second half of linked list
-        # 3. alternte between elements of first half and reversed second half
+        if not head:
+            return
 
-        #1. 
-        if not head: return
-
-        start_1 = head
-
-        # head starts 1 step ahead to make the mid point logic work for odd and even length
-        slow, fast, = head, head.next
-
-        while fast and fast.next: # don't need to check slow
+        # 1. Split the list in half.
+        # Starting `fast` one step ahead makes the split work cleanly for
+        # both even and odd lengths (left half ends up >= right half).
+        slow, fast = head, head.next
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        cur = slow.next
-        slow.next = None
+        first = head
+        second = slow.next
+        slow.next = None  # terminate the first half
 
-        print(cur)
-
-        #2. 
+        # 2. Reverse the second half in place.
         prev = None
-
+        cur = second
         while cur:
             nxt = cur.next
             cur.next = prev
             prev = cur
             cur = nxt
+        second = prev
 
-        start_2 = prev
-        # cur now contains Null and prev contains the first element of reversed list  
-        #3.
-
-        # note: list 2 is guarenteed to be the same size or shorter as list 1
-        while start_2:
-            nxt_1, nxt_2 = start_1.next, start_2.next
-
-            start_1.next = start_2
-            start_2.next = nxt_1
-            start_1 = nxt_1
-            start_2 = nxt_2
-
-        return head
+        # 3. Interleave the two halves.
+        # The second half is guaranteed to be the same length or shorter
+        # than the first, so we iterate until `second` is exhausted.
+        while second:
+            nxt_1, nxt_2 = first.next, second.next
+            first.next = second
+            second.next = nxt_1
+            first, second = nxt_1, nxt_2
