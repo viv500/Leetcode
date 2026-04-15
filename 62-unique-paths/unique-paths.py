bae139bedb0741234c974cfdf36a1e182f3d3d96
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-15 17:31:30 -0400

    Time: 4 ms (6.33%) | Memory: 19.3 MB (51.27%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,53 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # can only move right or down
+        # dfs problem, can be improved using a DP cache to store previous results
+        # if 2 paths are exactly same except for their first decision, dfs will have more calculations involves than dp
+        # dp relation: start from bottom right corner
+        # - for each cell, number of ways to get there = number of ways to get to the cell to the right + down (if inside the grid)
+
+        dp = [[0] * n for _ in range(m)]
+        dp[m - 1][n - 1] = 1
+
+        for row in range(m - 1, -1, -1):
+            for col in range(n - 1, -1, -1):
+                if (row, col) == (m - 1, n - 1): continue
+                
+                cell_value = 0
+                # has right cell?
+                if col + 1 < n:
+                    cell_value += dp[row][col + 1]
+
+                if row + 1 < m:
+                    cell_value += dp[row + 1][col]
+
+                dp[row][col] = cell_value
+
+
+        print(dp)
+        return dp[0][0]
+
+                
+
+
+# Standard DFS - Works but not efficient enough due to multiple duplicate calculations
+'''
+        paths = 0
+        # Must declare it as nonlocal to tell Python not to treat it as local.
+    
+
+        def dfs(r, c):
+            nonlocal paths
+            if r >= m or c >= n or r < 0 or c < 0: return
+
+            if (r, c) == (m - 1, n - 1):
+                paths += 1
+
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+
+        
+        dfs(0, 0)
+        return paths
+        '''
+        
