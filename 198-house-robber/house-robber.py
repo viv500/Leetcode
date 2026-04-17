010e1d10b8992f73ebc1bbb17c16c6540fb2e89d
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-17 17:54:01 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.98%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,27 +1,12 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 0
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
 
-        if len(nums) == 1:
-            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
-        nums[1] = max(nums[0], nums[1])
-
-        # most money that can be stolen at index i is max(money at index i + max mony from 2 indices ago, max money from 1 index ago and exclude current index)
-
-        for index in range(2, len(nums)):
-            nums[index] = max(
-                nums[index] + nums[index - 2],
-                nums[index - 1])
-
-        # Time Complexity: O(n)
-        # Space Complexity: O(1) (in-place DP)
-        # DP Type: Bottom-up dynamic programming with in-place tabulation
-
-        return nums[len(nums) - 1]
+        return dp[len(nums) - 1]
