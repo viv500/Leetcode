d2449e224a157f241a517f85bbcfbbe0e2412f53
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-20 13:13:51 -0400

    Time: 3 ms (91.00%) | Memory: 19.8 MB (81.96%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,15 +1,19 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_prod = nums[0]
+        # variation of kadane's algorithm, but need to worry about negative products
+
+        # can't simply discard negatives like regular kadanes since a negative could help in the future 
+
         min_prod = nums[0]
+        max_prod = nums[0]
         result = nums[0]
 
-        for n in nums[1:]:
-            if n < 0:
-                max_prod, min_prod = min_prod, max_prod
+        for num in nums[1:]:
+            if num < 0:
+                min_prod, max_prod = max_prod, min_prod
 
-            max_prod = max(n, max_prod * n)
-            min_prod = min(n, min_prod * n)
+            min_prod = min(num, min_prod * num)
+            max_prod = max(num, max_prod * num)
 
             result = max(result, max_prod)
 
