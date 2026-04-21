e4804fc2fdc172f4641371b3777a777f3200d062
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-21 14:36:19 -0400

    Time: 11 ms (76.95%) | Memory: 19.4 MB (11.75%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,15 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        L = R = 0
+        # note: we are removing s[left], always exists safe to remove
+        # keep removing left characters until we remove the right character that WOULD'VE been a duplicate
+        # then, add that right character
+        char_set = set()
         longest = 0
-        chars = set()
 
-        while R < len(s):
-            while s[R] in chars:
-                chars.remove(s[L])
-                L += 1
-            longest = max(longest, R - L + 1)
-            chars.add(s[R])
+        left, right = 0, 0
 
-            R += 1
+        while right < len(s):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            longest = max(longest, right - left + 1)
+
+            char_set.add(s[right])
+
+            right += 1
         return longest
