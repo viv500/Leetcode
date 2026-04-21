ee0a2c0185eccfa9094bcb1260f0f08b028d57c0
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-21 17:03:04 -0400

    Time: 246 ms (53.39%) | Memory: 21.5 MB (92.33%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,31 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
         island_count = 0
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-        def dfs(r, c):
-            stack = [(r, c)]
+        def bfs(r, c):
+            q = deque([(r, c)])
 
-            while stack:
-                r, c = stack.pop()
+            while q:
+                r, c = q.popleft()
 
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
                         grid[nr][nc] = "0"
-                        stack.append((nr, nc))
-
+                        q.append((nr, nc))
 
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1":
                     island_count += 1
-                    dfs(row, col)
+                    grid[row][col] = "0"
+                    bfs(row, col)
 
         return island_count
 
 
-        
+
