aa765113cc7fe7a23221397f5cd2d9a76e2e9fbf
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-26 12:45:59 -0400

    Add: decode-ways - submission-8

---
+++ Data Structures & Algorithms/decode-ways/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # dp[n] = number of ways to decode string upto (excluding) index n
+        # if a b c d n , then dp[n] = dp[a b c | d n] + dp[a b c d | n] (if dn is <= 26)
+        if s[0] == "0": return 0
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[n]
+
+        # optimization: since we only look back at 2 values, use 2 variables to store them instead of dp array
