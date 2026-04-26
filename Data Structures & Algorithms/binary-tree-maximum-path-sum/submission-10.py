bb6db46e758e2c816cb3906b52cddc6bf30180ec
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-26 18:41:37 -0400

    Add: binary-tree-maximum-path-sum - submission-10

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-10.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # brute force: for each node, calculate:
+        # longest path sum on left + node.val + longest path sum on right
+        # a lot of repeated calculations
+
+        # better approach that's O(n) time and O(log n) space
+        result = float('-inf')
+
+        def dfs(node):
+            nonlocal result
+            if not node: return 0
+
+            left_sum = max(0, dfs(node.left)) # handles case where child sum is negative
+            right_sum = max(0, dfs(node.right))
+
+            result = max(result, node.val, node.val + left_sum + right_sum)
+            # result assuming current node is the only "branch split"
+
+            return max(node.val, node.val + left_sum, node.val + right_sum)
+            # return to parent caller assuming current node is not the "branch split"
+            # i.e. return max straight path
+
+        dfs(root)
+
+        return result
