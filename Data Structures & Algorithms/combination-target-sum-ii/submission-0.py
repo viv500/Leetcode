f017b0e61394ec29e07599bcfdf4d5b0bcdd543a
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-26 21:49:09 -0400

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # combination sum 1: can use numbers as many times as needed
+        # combination sum 2: input array has dupes but each number can only be used once
+
+        # changes -> sort input array so dupes are adjacent
+        # if we find a dupe, skip all dupe indices and then recurse in the "skip branch"
+        # this will help avoid "duplicate results" ex target = 7, input = [1, 1, 6] => [[1, 6], [1, 6]]
+        candidates.sort()
+
+        output = []
+        combination = []
+
+        def dfs(i, summ):
+            # check this before the base case below so the last element isn't skipped
+            if summ == target:
+                output.append(combination.copy())
+                return
+
+            if summ > target or i >= len(candidates): return
+
+            number = candidates[i]
+
+            # take branch
+            combination.append(number)
+            dfs(i + 1, summ + number)
+
+            # skip branch
+            combination.pop()
+            j = i
+            while j < len(candidates) and candidates[j] == candidates[i]:
+                j += 1
+            dfs(j, summ)
+
+        dfs(0, 0)
+
+        return output 
