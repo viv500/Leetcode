a5739008592cdd85a109119dd2167ce04b405a2e
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-26 23:44:52 -0400

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        print(self.store)
+        values = self.store.get(key, [])
+        if len(values) == 0: return ""
+        if len(values) == 1: return values[0][0] if values[0][1] <= timestamp else ""
+
+        # binary sarch (for largest value smaller than timestamp)
+        low = 0
+        high = len(values) - 1
+        prev_timestamp = values[0][1]
+        while low <= high:
+            mid = (high + low) // 2
+
+            prev_timestamp = values[mid][1]
+            prev_value = values[mid][0]
+
+            if prev_timestamp <= timestamp:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        prev_timestamp = values[high][1]
+        prev_value = values[high][0]
+
+        return prev_value if prev_timestamp <= timestamp else ""
