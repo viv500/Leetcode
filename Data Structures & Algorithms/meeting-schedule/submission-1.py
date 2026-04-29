e0081aa6a6a48f0a78e4ef85867b7a9437269be7
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-29 19:50:06 -0400

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals: return True
+
+        # sorting an object with start property
+        intervals.sort(key = lambda x: x.start)
+
+        prev_start = intervals[0].start
+        prev_end = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < prev_end:
+                return False
+            else:
+                prev_start = interval.start
+                prev_end = interval.end
+
+        return True
+        
