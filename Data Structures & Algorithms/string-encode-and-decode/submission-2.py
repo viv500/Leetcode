e02af40cf8e9380dde44b1797b7f51a03778da97
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-29 19:07:07 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    # need to convert list of strings to string in such a way that it can be converted back into original string
+    # need a delimiter for this but the problem is the string can contain any character
+
+    # encode using the pattern count#wordcount#word
+    # ex. "love","you" is 4#love3#you
+    # now no matter what numbers are #'s are in the string, the 4# will skip over it and consider it in the word
+    # we can assume that deocde() only recieves strings that were encoded by encode()
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for word in strs:
+            result += (str(len(word)) + "#" + word)
+
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        output = []
+        i = 0
+
+        # note: count can be more than 1 character long
+
+        while i < len(s):
+            word = ""
+            count = 0
+            count = ""
+
+            while s[i] != "#":
+                count += s[i]
+                i += 1          
+
+            i += 1 # skip the #
+            for _ in range(int(count)):
+                word += s[i]
+                i += 1
+
+            print(word)
+            output.append(word)
+        
+        return output
