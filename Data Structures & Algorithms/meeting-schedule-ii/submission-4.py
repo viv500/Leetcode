661f9804f7012a065f7ddd408b374a5a1c1a54af
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-04-30 14:05:08 -0400

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,57 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # heap approach
+        if not intervals: return 0
+        intervals.sort(key = lambda x: x.start)
+        heap = [intervals[0].end]
+
+        for interval in intervals[1:]:
+            if heap and heap[0] <= interval.start:
+                heapq.heappop(heap)
+
+            heapq.heappush(heap, interval.end)
+
+        return len(heap)
+
+
+        # 2 pointer approach
+        # intuition: start with rooms = 0
+        # start < end can but does not always imply overlap
+        # it could also be the start and end of the same interval, in which case, 1 room still needs to be used!
+
+        start = []
+        end = []
+        rooms = 0
+        max_rooms = 0
+
+        for interval in intervals:
+            start.append(interval.start)
+            end.append(interval.end)
+
+        start.sort()
+        end.sort()
+
+        i, j = 0, 0
+
+        while i < len(start) and j < len(end):
+            # end >= start is no overlap, so 1 less room
+            if start[i] < end[j]:
+                rooms += 1
+                i += 1
+            else:
+                rooms -= 1
+                j += 1
+            
+            max_rooms = max(max_rooms, rooms)
+        
+        return max_rooms
+
+
