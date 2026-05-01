b1b5bacd84e7fa63071f20b480875bc5ba10ffba
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-01 14:34:40 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # binary SEARCH tree!
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+
+        # if a particular node is in betwen p and q, then it MUST be the LCA 
+        # since in a bst, this would mean p and q are on different sides. and any more branching means it no longer is a 
+        # common ancestor
+
+        cur = root
+
+        while cur:
+            if p.val <= cur.val <= q.val or p.val >= cur.val >= q.val: return cur
+
+            # if not, both p and q are on the same side (left or right)
+            if p.val <= cur.val:
+                cur = cur.left
+            else:
+                cur = cur.right
