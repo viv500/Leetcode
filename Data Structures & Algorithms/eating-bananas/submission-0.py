416b1deedec736ae771182416332686824b86c01
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-04 21:22:51 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # binary search on hourly rate, with ceiling being max(piles)
+        low = 1
+        high = max(piles)
+
+        def isFeasible(rate):
+            hours = 0
+            for pile in piles:
+                hours += ((pile + rate - 1) // rate) # integer way of doing ceil(pile // rate)
+
+            return hours <= h
+
+        
+        while low <= high:
+            mid = (low + high) // 2
+
+            if isFeasible(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        # this make sense since if mid was the final feasible rate, the else condition assigns low = mid + 1
+        return low
