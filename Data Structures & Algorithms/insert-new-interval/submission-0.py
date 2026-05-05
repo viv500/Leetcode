a3172f624ca9eed86dbe0acb0035977db0dbebf8
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-05 18:42:49 -0400

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # intervals is already sorted by start time
+
+        # 3 step process
+
+        # 1. add all intervals that have no overlap with newInterval
+        # 2. handle all the merge logic
+        # 3. add the remaining intervals
+
+        output = []
+        new_start, new_end = newInterval
+        index = 0
+        n = len(intervals)
+
+        # step 1
+        while index < n and intervals[index][1] < new_start:
+            output.append(intervals[index])
+            index += 1
+
+        # step 2
+        while index < n and new_end >= intervals[index][0]:
+            new_start = min(new_start, intervals[index][0])
+            new_end = max(new_end, intervals[index][1])
+            index += 1
+        
+        output.append([new_start, new_end])
+
+
+        # step 3
+        while index < len(intervals):
+            output.append(intervals[index])
+            index += 1
+        
+        return output
+        
+
+            
