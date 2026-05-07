fe2e150ca01e8db33e547c3073bb11c0ae3ab760
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-06 23:34:11 -0400

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # shortest sequence: bfs
+        # beginWord doesn't need to be in list, but all other (including end) should be in list
+
+        q = deque([(1, beginWord)])
+        visited = set()
+        wordList = set(wordList)
+    
+
+        while q:
+            step, word = q.popleft()
+
+            if word == endWord: return step
+
+            for i in range(len(word)):
+                for letter in "abcdefghijklmnopqrstuvwxyz":
+                    new_word = word[:i] + letter + word[i + 1:]
+                    if new_word in wordList and new_word not in visited:
+                        visited.add(new_word)
+                        q.append((step + 1, new_word))
+
+        return 0
+    
