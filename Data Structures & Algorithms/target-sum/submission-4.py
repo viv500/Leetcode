14258bb92b57e9d7d58855fd7dc0df12ed98a9cf
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-06 21:39:02 -0400

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # top down DP O(m * n) , same brute force but with caching
+        dp = {} # (index, cuSum) -> ways
+
+        def dfs(index, curSum):
+            if (index, curSum) in dp: return dp[(index, curSum)]
+            if index == len(nums):
+                return 1 if curSum == target else 0
+
+            dp[(index, curSum)] = (
+                dfs(index + 1, curSum + nums[index]) + dfs(index + 1, curSum - nums[index])
+            )
+
+            return dp[(index, curSum)]
+
+        
+        dfs(0, 0)
+
+        print(dp)
+        return dp[(0, 0)]
+
+
+        # brute force: + or - decision, O(2^n)
+
+        def dfs(index, curSum):
+
+            if index == len(nums):
+                return 1 if curSum == target else 0
+
+            return dfs(index + 1, curSum + nums[index]) + dfs(index + 1, curSum - nums[index])
+
+        
+        return dfs(0, 0)
+
+        
