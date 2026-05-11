695544a74e1c9ca596190fa445416a61066eca52
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-11 11:51:43 -0400

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+    
+
+        cache = {}
+
+        def dfs(i, j):
+            if j == len(t): return 1 # found the target!
+            if i == len(s): return 0 # reached end of s without finding target
+
+            if (i, j) in cache: return cache[(i, j)]
+
+            if s[i] == t[j]:
+                cache[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j) # even if theres a match, we should still try skipping
+                                                                          # ex. rabbbit and rabbit
+            else:
+                cache[(i, j)] = dfs(i + 1, j)
+
+            # for caching, always return value from cache here
+            return cache[(i, j)]
+
+        return dfs(0, 0)
