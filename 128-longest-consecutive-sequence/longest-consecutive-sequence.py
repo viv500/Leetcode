1cede26eac83c427308367d294e8cea08334dfa3
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-15 16:51:42 -0400

    Time: 35 ms (98.63%) | Memory: 36.8 MB (21.78%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,70 +1,14 @@
-class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        # idea: set() uses O(1) loop. confirm an element must be a starting element of a sequence before expanding. only 1 element will expand
-        # may look like O(n^2) but each element is visited exactly once! the inner loop runns for a sequence of element which will never be visited agiab
-        # this is because the inner loop only accesses all consecutive elements within the sequence. if a number isnt a part of this sequence, it will never viist   these again
-
-        num_set = set(nums) # O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
         longest = 0
-
-        for num in num_set:
-            if num - 1 not in num_set: # can be starting element
-                length = 1
-                cur = num
-                while cur + 1 in num_set:
-                    length += 1
-                    cur += 1
-                
-                longest = max(longest, length)
+        for num in nums:
+            if num - 1 not in nums:
+                size = 1
+                while num + 1 in nums:
+                    size += 1
+                    num += 1
+                longest = max(longest, size)
 
         return longest
-
-
-
-        # brute force: for each item -> search entire array for consecutive elements
-        #(O(n^2))
-
-        # longest = 0
-
-        # for num in nums:
-            # length = 1
-            # current = num
-
-            # while current + 1 in nums:
-                # current += 1
-                # length += 1
-            
-            # longest = max(longest, length)
-
-        # return longest
-
-
-        # slighty better: sort and then 2 pointer (nlogn)
-
-        # nums.sort()
-
-        # if not nums:
-            # return 0
-
-        # longest = 0
-        # current = 1
-        
-        # for i in range(1, len(nums)):
-            # if nums[i - 1] == nums[i]:
-                # continue
-
-            # if nums[i - 1] + 1 == nums[i]:
-                # current += 1
-            
-            # else:
-                # longest = max(longest, current)
-                # current = 1 # resent current
-
-        
-        # return max(longest, current)
 
