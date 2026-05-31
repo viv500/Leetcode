012dbb90a777bb05a0b46ddd91fac224d73cd90f
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-05-31 17:32:35 -0400

    Add: interleaving-string - submission-4

---
+++ Data Structures & Algorithms/interleaving-string/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        # !! NOTE: the "number of components" constraint means nothing
+        # if you reach the end of both strings, that condition is gaurenteed since you "interleaved"
+        if len(s1) + len(s2) != len(s3): return False # this prevents index out of boudns errors
+        dp = {}
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2): return True
+
+            if (i, j) in dp: return dp[(i, j)]
+
+            result = False
+            # index condition in case we consumed 1 string both not the other
+            if i < len(s1) and s1[i] == s3[i + j]:
+                result = dfs(i + 1, j)
+
+            if j < len(s2) and s2[j] == s3[i + j]:
+                result = dfs(i, j + 1)
+
+            dp[(i, j)] = result
+            return dp[(i, j)]
+
+        return dfs(0, 0)
