0370f68ee9c06f124397df6c49b5fa44e6d3760b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-06-24 19:04:06 -0400

    Time: 25 ms (55.37%) | Memory: 19.7 MB (81.10%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,26 +1,26 @@
 import heapq
-from collections import defaultdict
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        dist = [[float("inf") for _ in range(n)] for _ in range(n)] # this is distance from source to all nodes
-        dist[0][0] = grid[0][0]
+        # edge weight = max in path so far
+        rows, cols = len(grid), len(grid[0])
+        dist = [[float("inf")] * cols for _ in range(rows)]
         directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        dist[0][0] = grid[0][0]
 
-        minHeap = [(dist[0][0], (0, 0))]
-        graph = defaultdict(list)
-
-        print(dist)
+        minHeap = [(grid[0][0], 0, 0)]
 
         while minHeap:
-            weight, coords = heapq.heappop(minHeap)
-            r, c = coords
+            weight, r, c = heapq.heappop(minHeap)
+
+            if (r, c) == (rows - 1, cols - 1):
+                return weight
 
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
-                if 0 <= nr < n and 0 <= nc < n and dist[r][c] + max(weight, grid[nr][nc]) < dist[nr][nc]:
-                    dist[nr][nc] = max(weight, grid[nr][nc])
-                    heapq.heappush(minHeap, (dist[nr][nc], (nr, nc)))
+                if 0 <= nr < rows and 0 <= nc < cols and max(dist[r][c], grid[nr][nc]) < dist[nr][nc]:
+                    dist[nr][nc] = max(dist[r][c], grid[nr][nc])
+                    heapq.heappush(minHeap, (dist[nr][nc], nr, nc))
 
         
-        return dist[n - 1][n - 1]
+
+            
