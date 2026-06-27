26962a1b05713e46ef6056a1d0023b2d9b9b4a92
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-06-26 21:26:00 -0400

    Add: stone-game - submission-0

---
+++ Data Structures & Algorithms/stone-game/submission-0.py
@@ -0,0 +1,5 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # even length array tells us if alice first picks an odd index, all future choices must be odd
+        # and if first choice is even, all future choises are even
+        return True
