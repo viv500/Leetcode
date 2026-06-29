eaf0854554fdda3792244735ed391bcb92821f5b
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-06-28 21:29:01 -0400

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # the index to be inserted can be found by binary search if it doesnt find the actual value
+        # low stores this
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target: return mid
+            elif nums[mid] > target: high = mid - 1
+            else: low = mid + 1
+
+        return low
