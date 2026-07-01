15570875e78c421961417c0bbbc9e848fb37dc0f
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-06-30 21:09:16 -0400

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char == "]":
+                char = ""
+                while stack[-1] != "[":
+                    char = stack.pop() + char
+                
+                stack.pop()
+            
+                
+                count = ""
+                # all remainign characters after [ are the count
+                while stack and stack[-1].isdigit():
+                    count = stack.pop() + count
+                   
+                # has to be digit here
+                mult = int(count)
+                char *= mult
+
+
+     
+            stack.append(char)
+    
+        return "".join(stack)
+            
+        
