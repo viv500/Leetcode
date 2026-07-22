1da905de84350fceb199413a1f7c00020d076902
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2026-07-22 16:09:53 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,15 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(1, len(intervals)):
+            if intervals[i - 1].end > intervals[i].start: return False
+        return True
